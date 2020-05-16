48cce82f0762ada5734c6541cea17266e3f73f0f
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-16 15:39:50 +0530

    create int[] type PRECEDENCE

--- src/defination/InfixToPostfix.java
+++ src/defination/InfixToPostfix.java
@@ -8,6 +8,7 @@ import java.util.Deque;
 public class InfixToPostfix implements InfixToPostfixADT {
     private final Deque<Character> operatorStack = new ArrayDeque<>();
     private final String OPERATORS = "+-*/^";
+    private final int[] PRECEDENCE = {1, 1, 2, 2, 3};
     @Override
     public void convertToPostfix(String infix) {
 
