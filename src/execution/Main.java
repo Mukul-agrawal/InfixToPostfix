c065610c86fec3bcce31db536eb3c90ebc38bd65
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-16 17:50:26 +0530

    create InfixToPostfix class object

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,10 +1,13 @@
 package execution;
 
+import defination.InfixToPostfix;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        InfixToPostfix infixToPostfix = new InfixToPostfix();
 
     }
 }
