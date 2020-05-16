1ac645f234a1022bbdb1ef4aebb1f8edfb440606
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-16 15:40:49 +0530

    create StringJoiner class object

--- src/defination/InfixToPostfix.java
+++ src/defination/InfixToPostfix.java
@@ -4,11 +4,13 @@ import adt.InfixToPostfixADT;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.StringJoiner;
 
 public class InfixToPostfix implements InfixToPostfixADT {
     private final Deque<Character> operatorStack = new ArrayDeque<>();
     private final String OPERATORS = "+-*/^";
     private final int[] PRECEDENCE = {1, 1, 2, 2, 3};
+    private StringJoiner postfix = new StringJoiner(" ");
     @Override
     public void convertToPostfix(String infix) {
 
