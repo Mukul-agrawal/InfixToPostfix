44d7919fd531acb9b930c1b7190af4438aa44d3f
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-16 17:51:40 +0530

    create infix variable

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         InfixToPostfix infixToPostfix = new InfixToPostfix();
+        System.out.println("Enter the infix expression :");
+        String infix = scanner.nextLine();
 
     }
 }
