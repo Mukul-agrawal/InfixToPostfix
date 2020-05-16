a6d896bc9bfed1a7771a16c1a673afdcf9df68f6
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-16 15:33:09 +0530

    implement InfixToPostfixADT interface and override all methods

--- src/defination/InfixToPostfix.java
+++ src/defination/InfixToPostfix.java
@@ -1,4 +1,30 @@
 package defination;
 
-public class InfixToPostfix {
+import adt.InfixToPostfixADT;
+
+public class InfixToPostfix implements InfixToPostfixADT {
+    @Override
+    public void convertToPostfix(String infix) {
+
+    }
+
+    @Override
+    public void processOperator(char op) {
+
+    }
+
+    @Override
+    public boolean isOperator(char ch) {
+        return false;
+    }
+
+    @Override
+    public int precedence(char op) {
+        return 0;
+    }
+
+    @Override
+    public String getPostfix() {
+        return null;
+    }
 }
